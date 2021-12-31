f42ba59d7e76a39a4b6637c531fa09fa189323e9
juliano Ferreira <julianooen@hotmail.com>
2021-12-31 10:30:52 -0300

    add mensagens de confirmacao

--- Cadastro 1 com bootstrap/java.js
+++ Cadastro 1 com bootstrap/java.js
@@ -1,38 +1,50 @@
 var tabela, index;
 
 function cadastrarNovo(nome, fone, nDas, nota){
-    if(nome==''|fone==''|nDas==''|nota==''){return alert('Campos nome, telefone, data de nascimento e nota são obrigatórios!')}
-    tabela = window.document.querySelector('#tCad');
-    let numLinhas = tabela.rows.length;
-    let novaLinha = tabela.insertRow(numLinhas);
-    
-    let cellNome = novaLinha.insertCell(0);
-    let cellFone = novaLinha.insertCell(1);
-    let cellData = novaLinha.insertCell(2);
-    let cellNota = novaLinha.insertCell(3);
+    if(window.document.querySelector('#incluirBtn').innerHTML == 'Cadastrar Novo'){
 
-    cellNome.innerHTML = nome;
-    cellFone.innerHTML = fone;
-    cellData.innerHTML = nDas;
-    cellNota.innerHTML = nota;
-    alert('Aluno cadastrado com sucesso!');
-    preencher();
-    limpaCampos();
+        if(nome==''|fone==''|nDas==''|nota==''){return alert('Campos nome, telefone, data de nascimento e nota são obrigatórios!')}
+        
+        tabela = window.document.querySelector('#tCad');
+        
+        let numLinhas = tabela.rows.length;
+        let novaLinha = tabela.insertRow(numLinhas);
+        let cellNome = novaLinha.insertCell(0);
+        let cellFone = novaLinha.insertCell(1);
+        let cellData = novaLinha.insertCell(2);
+        let cellNota = novaLinha.insertCell(3);
+
+        cellNome.innerHTML = nome;
+        cellFone.innerHTML = fone;
+        cellData.innerHTML = nDas;
+        cellNota.innerHTML = nota;
+        
+        alert('Aluno cadastrado com sucesso!');
+        preencher();
+        limpaCampos();
+    }else{
+        
+        limpaCampos();
+    }
 }
 
 function atualizar(nome, fone, nDas, nota){
-
-    if(nome==''|fone==''|nDas==''|nota==''|index==0){return alert('Você primeiro deve clicar sobre o registro que deseja atualizar')}
-    tabela.rows[index].cells[0].innerHTML = nome;
-    tabela.rows[index].cells[1].innerHTML = fone;
-    tabela.rows[index].cells[2].innerHTML = nDas;
-    tabela.rows[index].cells[3].innerHTML = nota;
-    limpaCampos();
+    if(confirm('Deseja realmente atualizar este registro?')){
+        if(nome==''|fone==''|nDas==''|nota==''|index==0){return alert('Você primeiro deve clicar sobre o registro que deseja atualizar')}
+        tabela.rows[index].cells[0].innerHTML = nome;
+        tabela.rows[index].cells[1].innerHTML = fone;
+        tabela.rows[index].cells[2].innerHTML = nDas;
+        tabela.rows[index].cells[3].innerHTML = nota;
+        
+        alert('Aluno atualizado com sucesso!')
+        limpaCampos();
+    }
     
 }
 function preencher(){
     for(let i = 0;i<tabela.rows.length;i++){
         tabela.rows[i].onclick = function(){
+            window.document.querySelector('#incluirBtn').innerHTML = 'Cancelar'
             index = this.rowIndex;
             window.document.getElementById('nome').value = tabela.rows[index].cells[0].innerText;
             window.document.getElementById('fone').value = tabela.rows[index].cells[1].innerText;
@@ -43,11 +55,13 @@ function preencher(){
 }
 
 function deletar(nome, fone, nDas, nota){
-    if(nome==''|fone==''|nDas==''|nota==''){return alert('Você primeiro deve clicar sobre o registro que deseja excluir')
-    }else if(confirm('Deseja realmente excluir este registro?')==true){
+    if(nome==''|fone==''|nDas==''|nota==''|index==0){return alert('Você primeiro deve clicar sobre o registro que deseja excluir')
+    }else if(confirm('Deseja realmente excluir este registro?')){
         for(let i = 0;i<tabela.rows.length;i++){
             if(index == i &&index!=0){
                 tabela.deleteRow(index);
+                
+                alert('Aluno excluido com sucesso!')
                 limpaCampos();
             }else if (index==0){return alert('escolha um campo válido!')}
         }
@@ -62,4 +76,5 @@ function limpaCampos(){
     window.document.getElementById('nDas').value = '';
     window.document.getElementById('nota').value = '';
     index=0;
+    window.document.querySelector('#incluirBtn').innerHTML = 'Cadastrar Novo'
 }
